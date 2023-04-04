56a4f8dc390cd4c0a353925823f5e89915062192
Benjamin Gorny <ben.gorny@web.de>
2023-04-04 16:57:27 +0200

    update deps & add test for app

--- applications/backend/src/main/java/searchrequests/dto/UiApplicationDto.java
+++ applications/backend/src/main/java/searchrequests/dto/UiApplicationDto.java
@@ -5,7 +5,6 @@ import searchrequests.model.Salutation;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
---
+++ applications/backend/src/test/java/searchrequests/ApplicationTest.java
@@ -0,0 +1,13 @@
+package searchrequests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationTest {
+
+    @Test
+    @DisplayName("test spring boot context creation")
+    void testContextLoads() {}
+}
